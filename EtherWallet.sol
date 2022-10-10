11335c46296a4ed037d97a68aec8a5359ebb3151
Adetutu <adetutugbangbola@gmail.com>
2022-10-10 19:27:44 +0100

    first commit

---
+++ EtherWallet.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.13;
+
+contract SendEther {
+
+      event Deposit ( address from, uint amount, uint timestamp );
+
+      event Withdrawal ( uint amount, uint timestamp );
+        
+
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+
+      modifier onlyOwner() {
+        require(msg.sender == owner, "sorry, only owner can withdraw");  
+        _;
+    }
+
+// to deposit ether
+    function StoreEther(uint amount) payable external{
+        require(amount == msg.value, "amount not match value");
+        payable(address(this)).transfer(amount);
+        emit Deposit(msg.sender, msg.value, block.timestamp);
+    }
+
+// to withdraw ether
+    function Withdraw(uint _amount)  external onlyOwner {
+        require(_amount <= address(this).balance, "sorry, insufficient funds");
+        payable(msg.sender).transfer(_amount);
+        emit Withdrawal(_amount, block.timestamp);
+    }
+
+
+// to check balance
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+
+#### This is a wallet application that allows any user to store, send and deposit ether.
+
+##### Features:
+
+- Custom Error messages
+- Use of events 
